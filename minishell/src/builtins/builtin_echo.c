f870da131f2a85fb4cbd429b81856047cc601af0
Bernardo Conejo <bconejo-@student.42malaga.com>
2026-07-23 21:46:54 +0200

    refactor: simplify echo command logic and improve readability

--- minishell/src/builtins/builtin_echo.c
+++ minishell/src/builtins/builtin_echo.c
@@ -16,7 +16,19 @@ static t_status	check_arg(char *str)
 	return (SUCCESS);
 }
 
+static void	print_word_and_space(char **str, int *i)
+{
+	ft_putstr_fd(str[*i], 1);
+	(*i)++;
+	if (str[*i])
+		ft_putchar_fd(32, 1);
+}
 
+static void print_endl(t_shell *msh)
+{
+	if (!(msh->cmd->arg[1] && !ft_strncmp(msh->cmd->arg[1], "-n", 2)))
+		ft_putchar_fd(10, 1);
+}
 
 /**
  * @brief Echo the STRING(s) to standard output
@@ -36,27 +48,18 @@ void	builtin_echo(t_shell *msh)
 		if (!ft_strncmp(msh->cmd->arg[i], "-n", 2))
 		{
 			if (check_arg(msh->cmd->arg[i]) && test_arg)
-			{
 				i++;
-			}
 			else
 			{
 				test_arg = 0;
-				ft_putstr_fd(msh->cmd->arg[i++], 1);
-				if (msh->cmd->arg[i])
-					ft_putchar_fd(32, 1);
+				print_word_and_space(msh->cmd->arg, &i);
 			}
 		}
 		else
 		{
 			while (msh->cmd->arg[i])
-			{
-				ft_putstr_fd(msh->cmd->arg[i++], 1);
-				if (msh->cmd->arg[i])
-					ft_putchar_fd(32, 1);
-			}
+				print_word_and_space(msh->cmd->arg, &i);
 		}
 	}
-	if (!(msh->cmd->arg[1] && !ft_strncmp(msh->cmd->arg[1], "-n", 2)))
-		ft_putchar_fd(10, 1);
+	print_endl(msh);
 }
--- minishell/src/parser/parser_utils.c
+++ minishell/src/parser/parser_utils.c
@@ -9,25 +9,24 @@
 void	print_syntax_error(t_token *token)
 {
 	if (!token)
-		write(2, "minishell: syntax error near unexpected token `newline`\n",
-			57);
+		ft_putendl_fd("minishell: syntax error near unexpected token `newline`",
+			2);
 	else if (!token->value)
 	{
 		if (token->type == PIPE)
-			write(2, "minishell: syntax error near unexpected token `|`", 51);
+			ft_putstr_fd("minishell: syntax error near unexpected token `|`", 2);
 		else if (token->type == INPUT)
-			write(2, "minishell: syntax error near unexpected token `<`", 51);
+			ft_putstr_fd("minishell: syntax error near unexpected token `<`", 2);
 		else if (token->type == TRUNC)
-			write(2, "minishell: syntax error near unexpected token `>`", 51);
+			ft_putstr_fd("minishell: syntax error near unexpected token `>`", 2);
 		else if (token->type == APPEND)
-			write(2, "minishell: syntax error near unexpected token `>>`", 52);
+			ft_putstr_fd("minishell: syntax error near unexpected token `>>`", 2);
 		else if (token->type == HEREDOC)
-			write(2, "minishell: syntax error near unexpected token `<<`", 52);
+			ft_putstr_fd("minishell: syntax error near unexpected token `<<`", 2);
 	}
 	else
 	{
-		write(2, "minishell: syntax error near unexpected token `", 48);
-		write(2, token->value, ft_strlen(token->value));
-		write(2, "`\n", 2);
+		ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+		ft_putendl_fd(token->value, 2);
 	}
 }
